867743382107ac5c6c725a580e18d150ffe4913f
Nicat Isayev <nijat@Nicats-MacBook-Air.local>
2022-04-18 19:50:17 +0400

    get envs from configmap

--- k8s_operator.py
+++ k8s_operator.py
@@ -1,3 +1,4 @@
+from traceback import print_tb
 import kopf
 from kubernetes import kubernetes
 
@@ -10,12 +11,41 @@ async def my_handler(body,spec, **kwargs):
     body = body.__dict__["_src"]
     del body["metadata"]["resourceVersion"]
     container = body["spec"]["template"]["spec"]["containers"][0]
+    envs = [{
+                "name": "HTTP_PROXY",
+                    "valueFrom":{
+                        "configMapKeyRef": {
+                            "key": "http_proxy",
+                            "name": "nginx-cm"
+                        }
+                    }
+            },
+            {
+                "name": "HTTP_PROXYS",
+                    "valueFrom":{
+                        "configMapKeyRef": {
+                            "key": "https_proxy",
+                            "name":"nginx-cm"
+                        }
+                    }
+            },
+            {
+                "name": "NO_PROXY",
+                    "valueFrom":{
+                        "configMapKeyRef": {
+                            "key": "no_proxy",
+                            "name":"nginx-cm"
+                        }
+                    }
+            }]
     if container.get("env"):
-        container["env"].append({"name": "http_proxy","value": "http://153.95.94.130:8080"},{"name": "https_proxy","value":"http://153.95.94.130:8080"},{"name": "no_proxy","value": "localhost,127.0.0.1,.svc,.svc.cluster.local,.cluster.local,.flux-system,.flux-system.svc.cluster.local,10.103.82.0/24,10.36.0.0/24,10.44.0.0/24,10.96.0.0/12,10.219.132.14,nexus.app.nox.express,gitlab.dev.nox.express,10.219.132.12,10.96.0.1,10.219.1.51,10.219.1.52,10.219.1.53,10.219.1.54,10.219.1.55,10.219.1.56,10.219.1.14,10.219.3.22,10.219.3.24,10.219.3.25,*.de.innight.tnt,*.dev.nox.express,*.shared.nox.express,*.int.nox.express,*.app.nox.express,*.p-app.nox.express"})
+        container["env"].append(*envs)
     else:
-        container["env"] = [{"name": "http_proxy","value": "http://153.95.94.130:8080"},{"name": "https_proxy","value":"http://153.95.94.130:8080"},{"name": "no_proxy","value": "localhost,127.0.0.1,.svc,.svc.cluster.local,.cluster.local,.flux-system,.flux-system.svc.cluster.local,10.103.82.0/24,10.36.0.0/24,10.44.0.0/24,10.96.0.0/12,10.219.132.14,nexus.app.nox.express,gitlab.dev.nox.express,10.219.132.12,10.96.0.1,10.219.1.51,10.219.1.52,10.219.1.53,10.219.1.54,10.219.1.55,10.219.1.56,10.219.1.14,10.219.3.22,10.219.3.24,10.219.3.25,*.de.innight.tnt,*.dev.nox.express,*.shared.nox.express,*.int.nox.express,*.app.nox.express,*.p-app.nox.express"}]
+        container["env"] = envs
     body["spec"]["template"]["spec"]["containers"][0] = container
     api = kubernetes.client.AppsV1Api()
     api.patch_namespaced_deployment(name,namespace,body)
     print(f"Deployment {name} successfully patched")
+    print(body)
+
     return {"msg": f"Successed deployed {name}"}
